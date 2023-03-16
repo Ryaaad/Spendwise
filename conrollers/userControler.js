22651c36e4dcfd2d9bff360bbeba5a31ca06805e
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-03-16 01:25:49 +0100

    Users Delete Req

--- conrollers/userControler.js
+++ conrollers/userControler.js
@@ -74,108 +74,37 @@ export async function patchHandler(req,res){
     }
 }
 
-// export async function postHandler(req,res){
-//     try{
-//         const {motif,qte,product,prixV,prixHt}=req.body
-//     if(!motif || !qte || !product || !prixV) return res.status(400).json({status:400,message:"missing data"})
-//     const newSortieStock=await prisma.sortieStock.create({
-//         data:{
-//             motif,qte,prixV,prixHt,
-//             product:{
-//                 connect:{
-//                     codeP:product*1
-//                 }
-//             }
-//         }
-//     });
-//     return res.status(201).json({
-//         status:201,
-//         data:newSortieStock
-//     })
-//     }
-//     catch(err){
-//         console.error(err)
-//         return res.status(500).json({status:500,message:"an error occurred"})
-//     }
-// }
-// export async function putHandler(req,res){
-//     try{
-//         const {id}=req.query
-//         if(!id) return res.status(400).json({status:400,message:"INvalid id"})
-//         const sortieStock=await prisma.sortieStock.findUnique({
-//             where:{
-//                 idSortieStock:id*1
-//             }
-//         });
-//     if(!sortieStock)return res.status(404).json({status:404,message:"no sortieStocks found"})
-//     let {motif,qte,product,prixV,prixHt}=req.body
-//     if(!motif && !qte && !product && !prixV) return res.status(400).json({status:400,message:"missing data"})
 
-//         if(!qte)qte=sortieStock.qte
-//         if(!motif)motif=sortieStock.qte
-//         if(!product)product=sortieStock.produit
-//         if(!prixV)prixV=sortieStock.prixV
-//         if(!prixHt)prixHt=sortieStock.prixHt
-//     const sortieStockUpdated=await prisma.sortieStock.update({
-//         where:{
-//             idSortieStock:id*1
-//         },
-//         data:{
-//             motif,qte,produit:{connect:{
-//                 codeP:product
-//             }},prixV,prixHt
-//         }
-//     });
-    
-//     return res.status(200).json({
-//         status:200,
-//         data:sortieStockUpdated
-//     })
-//     }
-//     catch(err){
-//         console.error(err)
-//         return res.status(500).json({status:500,message:"an error occurred"})
-//     }
-// }
-
-// export async function deleteHandler(req,res){
-//     try{
-//         const {id}=req.query
-//         if(!id) return res.status(400).json({status:400,message:"INvalid Name"})
-//         const sortieStock=await prisma.sortieStock.findUnique({
-//             where:{
-//                 idSortieStock:id*1
-//             }
-//         });
-
-//     if(!sortieStock)return res.status(404).json({status:404,message:"no sortieStock found"})
-//     const newproductstock=await prisma.productstock.create({
-//         data:{
-//             qte:sortieStock.qte,
-//             prixV:sortieStock.prixV,
-//             prixHt:sortieStock.prixHt,
-//             product:{
-//                 connect:{
-//                     codeP:sortieStock.produit
-//                 }
-//             }
-//         }
-//     });
-//     if(!newproductstock) return res.status(400).json({status:400,message:"error creating new product in stock"})
-//     const sortieStockDeleted=await prisma.sortieStock.delete({
-//         where:{
-//             idSortieStock:id*1
-//         }
-//     });
-    
-    
-
-//     return res.status(204).json({
-//         status:204
-//     })
-//     }
-//     catch(err){
-//         console.error(err)
-//         return res.status(500).json({status:500,message:"an error occurred"})
-//     }
-// }
+export async function deleteHandler(req, res) {
+    try {
+      const { id } = req.query;
+  
+      if (!id) {
+        return res.status(400).json({ status: 400, message: "Invalid ID" });
+      }
+  
+      const existingProduct = await prisma.User.findUnique({
+        where: {
+            id: parseInt(id),
+        },
+      });
+  
+      if (!existingProduct) {
+        return res
+          .status(404)
+          .json({ status: 404, message: "User not found" });
+      }
+  
+      const deletedUser= await prisma.User.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+  
+      return res.status(204).send()
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ status: 500, message: "An error occurred" });
+    }
+  }
+  
